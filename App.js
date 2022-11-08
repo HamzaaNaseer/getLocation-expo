8408105f6ee1dd019759a28a50d344df84b2ce0d
hamza naseer <hamzanaseer711@gmail.com>
2022-11-08 22:21:23 +0500

    live location showing on the map

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+//location package
+import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
+
+//maps import
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync(); // this asks for permission
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location && (
+        <MapView
+          style={{ width: "100%", height: "80%" }}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.04,
+            longitudeDelta: 0.05,
+          }}
+        >
+          {<Marker coordinate={location.coords} />}
+        </MapView>
+      )}
+
+      {location && (
+        <View style={styles.locationTextContainer}>
+          <Text style={styles.locationText}>
+            Your Latitude {location.coords.latitude}
+          </Text>
+          <Text style={styles.locationText}>
+            Your longitude {location.coords.longitude}
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +58,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+  locationTextContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  locationText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#f6cd61",
   },
 });
